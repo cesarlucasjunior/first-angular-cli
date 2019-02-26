f1b7b17247a792c03405e7c2a338c6ddf93a3cc1
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-02-26 13:51:49 -0300

    Criação do componente task-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tasks';
-  tasks = [];
-  task = "";
-  add(): void{
-    this.tasks.push(this.task);
-  }
+  title = 'Tarefas';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FormsModule} from '@angular/forms';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
