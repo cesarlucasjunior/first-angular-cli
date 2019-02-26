49a53ab87b76513ce748b434070eac2102b8b165
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-02-26 14:30:54 -0300

    Inclusão de pipes - json, upperCase, lowerCase, percentual, date, currency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tarefas';
+
+  upperText:string = 'Testando upper case';
+  lowerText:string = 'Testando lower case';
+  percentValue:number = 0.5;
+  date:Date = new Date();
+  moeda:number = 598;
+  user:User = {
+    nome: 'Bob',
+    age: 25
+  }
 }
